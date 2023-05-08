8f78c38c3ed1cdef6de4394149085bbcb03eb3d3
vishal sutar <vishal@prismbiomed.in>
2023-05-09 00:00:14 +0530

    tes changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -115,7 +115,7 @@ export default function Home() {
           <p
             className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
           >
-            Instantly deploy your Next.js site to a shareable URL with Vercel.Thanks...
+            Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
         </a>
       </div>
